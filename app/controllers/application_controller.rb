75ac303cd4c1d7bd2d5c20cbac63194d0809a8a4
Naya Willis <greedybrain18@gmail.com>
2020-01-03 22:45:51 -0500

    set folder to public folder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < Sinatra::Base
 
     configure do 
         set :views, "app/views"
+        set :public_folder, File.join(APP_ROOT, "public")
         enable :sessions
         set :session_secret, "s7y7n7t7a7x7t7i7c"
     end
